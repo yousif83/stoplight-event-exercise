664334a95f882afeefd3bc7f2645bfa090aed801
yousif al timimie <yousifaltimimie@yousifs-MacBook-Air.local>
2017-06-13 00:24:59 -0600

    finish part2

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 (function() {
   'use strict';
+  toggleLights("stopButton","stopLight","stop");
+  toggleLights("slowButton","slowLight","slow");
+  toggleLights("goButton","goLight","go");
 
-  // YOUR CODE HERE
+  logButtonContent("stopButton");
+  logButtonContent("slowButton");
+  logButtonContent("goButton");
+
+
+function toggleLights(ButtonElement,light,action){
+    var ButtonElements = document.getElementById(ButtonElement);
+    var lights = document.getElementById(light);
+    ButtonElements.addEventListener("click", function(){
+    lights.classList.toggle(action);
+    });
+}
+function logButtonContent(ButtonElement){
+     var ButtonElements = document.getElementById(ButtonElement);
+       ButtonElements.addEventListener("mouseenter", function( event ) {
+       console.log("Entered ",event.target.innerHTML," button")
+       });
+       ButtonElements.addEventListener("mouseleave", function( event ) {
+       console.log("Left ",event.target.innerHTML," button")
+       });
+}
+
+
 })();
